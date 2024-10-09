e173e1636acb2ad51a564129cb75ceef2f0ed96f
lai2-24 <lai2-24@itismeucci.com>
2024-10-09 08:51:56 +0200

    client

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,14 @@
 package com.example;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnknownHostException, IOException {
         System.out.println("Il client è partito");
+
+        Socket mySocket = new Socket("localhost", 5637);
+        System.out.println("Il client si è collegato");
     }
 }
