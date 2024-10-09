f4ca2995369bbe1e3e18747e3e877d5c5758033c
lai2-24 <lai2-24@itismeucci.com>
2024-10-09 09:35:09 +0200

    client completo

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,6 +1,12 @@
 package com.example;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
@@ -10,5 +16,15 @@ public class Main {
 
         Socket mySocket = new Socket("localhost", 5637);
         System.out.println("Il client si è collegato");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(mySocket.getInputStream())); //ascolto (ricevere)
+        DataOutputStream out = new DataOutputStream(mySocket.getOutputStream()); //parla (invia)
+
+        out.writeBytes("ciao" + "\n"); //invio al server della stringa
+        String stringaRicevuta = in.readLine(); //in attessa della risposta dal server
+
+        System.out.println("La stringa ricevuta: " + stringaRicevuta); //stringa ricevuta e stampata in maiuscolo
+
+        mySocket.close(); //chiusura
     }
 }
